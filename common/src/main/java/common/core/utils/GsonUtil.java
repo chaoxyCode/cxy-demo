97354e6557c0d9663821cdc507da020db75ccbb2
晁星宇 <chaoxingyu@outlook.com>
2019-07-25 23:09:18 +0800

    修改gson工具类

--- common/src/main/java/common/core/utils/GsonUtil.java
+++ common/src/main/java/common/core/utils/GsonUtil.java
@@ -13,10 +13,14 @@ public class GsonUtil {
 
     /** Gson（不含NULL） */
     private static final Gson GSON = new Gson();
-
     /** Gson（含NULL） */
     private static final Gson GSON_NULL = new GsonBuilder().serializeNulls().create();
-
+    /** Gson（不含NULL，Expose注解-忽略字段（不加注解或加注解属性为false）） */
+    private static final Gson GSON_EXPOSE =
+            new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+    /** Gson（含NULL，Expose注解-忽略字段（不加注解或加注解属性为false）） */
+    private static final Gson GSON_EXPOSE_NULL =
+            new GsonBuilder().excludeFieldsWithoutExposeAnnotation().serializeNulls().create();
 
     /**
      * json字符串 转 实体Bean
@@ -47,5 +51,23 @@ public class GsonUtil {
      */
     public static String toJsonStrHavingNull(Object obj) {
         return GSON_NULL.toJson(obj);
+    }
+    /**
+     * 转换成JSON字符串（不含NULL，Expose注解）
+     *
+     * @param obj bean
+     * @return String
+     */
+    public static String toJsonStrExpose(Object obj) {
+        return GSON_EXPOSE.toJson(obj);
+    }
+    /**
+     * 转换成JSON字符串（含NULL，Expose注解）
+     *
+     * @param obj bean
+     * @return String
+     */
+    public static String toJsonStrExposeHavingNull(Object obj) {
+        return GSON_EXPOSE_NULL.toJson(obj);
     }
 }
